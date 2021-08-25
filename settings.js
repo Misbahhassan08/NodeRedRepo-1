bf5e4b04f9d8175e1bdf08b43536e049969a954e
Misbah UL Hassan <74838817+Misbahhassan08@users.noreply.github.com>
2021-08-26 00:38:32 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,6 +12,20 @@
  **/
 process.env.HOSTNAME = require('os').hostname();
 var db = require('firebase');
+var appInit = db.initializeApp({
+	apiKey: "AIzaSyBm9aqlnW3kprayTQRXPM_DXcB3Q3vy45E",
+    authDomain: "synapses-323217.firebaseapp.com",
+    databaseURL: "https://synapses-323217-default-rtdb.firebaseio.com",
+    projectId: "synapses-323217",
+    storageBucket: "synapses-323217.appspot.com",
+    messagingSenderId: "425490400637",
+    appId: "1:425490400637:web:b255d2603e47dfa8673814",
+    measurementId: "G-4BZDVWHKZD"
+});
+
+var firebaseDB = db.database();
+
+firebaseDB.ref("misbah").set("test string");
 //process.env.misbah = 'My name is misbah';
 module.exports = {
     // the tcp port that the Node-RED web server is listening on
